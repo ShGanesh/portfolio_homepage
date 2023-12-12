a2154066b77e443dc98ee8bc8a019e6488f2b915
ShGanesh <mc.sharen.ganesh@gmail.com>
2023-12-13 00:37:19 +0530

    corrected PDF errors

--- app.py
+++ app.py
@@ -90,7 +90,6 @@ st.markdown(os.listdir("./files"))
 
 st.markdown("I'm MC Sharen Ganesh, a passionate Machine Learning and LLM enthusiast.")
 st.markdown("I'm skilled in stuff here and there.")
-displayPDF()
 st.download_button("Download my resume", data = getPDF(), file_name="ShGanesh_Resume.pdf", mime='application/octet-stream') 
 st.markdown("Check out my projects below.")
 
